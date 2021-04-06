ab6f64ac21ef8f866f4a028117b503835ec1828c
Hengyu Liang <lhy1210302421@mail.ustc.edu.cn>
2021-04-06 08:04:36 +0800

    新增init.c文件（不含解决Kernel Panic功能）

---
+++ lab1/linux/src/init.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/sysmacros.h>
+#include <sys/wait.h>
+
+int main() {
+    if (mknod("/dev/ttyS0", S_IFCHR | S_IRUSR | S_IWUSR, makedev(4, 64)) == -1) {
+        perror("mknod() failed");
+    }
+    if (mknod("/dev/ttyAMA0", S_IFCHR | S_IRUSR | S_IWUSR, makedev(204, 64)) == -1) {
+        perror("mknod() failed");
+    }
+    if (mknod("/dev/fb0", S_IFCHR | S_IRUSR | S_IWUSR, makedev(29, 0)) == -1) {
+        perror("mknod() failed");
+    }
+    //printf("Start!");
+    for (int i = 1; i <= 3; i++){
+	    pid_t fpid = fork();
+	    if (fpid == 0){
+		    if (i == 1){
+			    execl("./1", "1", NULL);
+		    }
+		    if (i == 2){
+			    execl("./2", "2", NULL);
+		    }
+		    if (i == 3){
+			    execl("./3", "3", NULL);
+		    }
+	    }
+	    waitpid(fpid, NULL, 0);
+    }
+    //pid_t fpid;
+    //fpid = fork();
+    //if (fpid == 0){
+    //    execl("./1", "1", NULL);
+    //}
+    return 0;
+}
